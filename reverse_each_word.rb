42e76a03b14fa780661b0e14ba5b13520c0f168e
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-04 05:12:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  stringArray = string.split( )
+  stringArray = string.split(" ")
   stringArray.map do |letter|
     stringArray << stringArray[0]
     stringArray.shift
